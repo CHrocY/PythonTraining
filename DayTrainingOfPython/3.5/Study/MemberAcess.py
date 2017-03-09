656b2e7f79a48f471572f0974d71e0c39a99a51e
CHrocY <13308196164@163.com>
2017-03-09 15:08:23 +0800

    Add some notes in 'MemberAcess.py'.

--- DayTrainingOfPython/3.5/Study/MemberAcess.py
+++ DayTrainingOfPython/3.5/Study/MemberAcess.py
@@ -17,21 +17,21 @@ class ArithmeticSequence:
 		if key < 0: raise IndexError
 
 	def __getitem__(self, key):
-		"""获取值的时候自动调用这个方法"""
+		"""返回与所给的key对应的值"""
 		ArithmeticSequence.checkIndex(key)
 		# 尝试获取值
 		try: return self.changed[key]
 		# 没有值就计算值
 		except KeyError:
-			print("calculate new value")
+			print("Calculate new value")
 			return self.start + key*self.step
 
 	def __setitem__(self, key, value):
 		"""给这个实例化的类赋值的时候自动调用这个方法"""
 		ArithmeticSequence.checkIndex(key)
 		self.changed[key] = value
+		print(self.changed)
 
-s = ArithmeticSequence(10, 2)
-print(s[4])
-s[4] = 1
-print(s[4])
+s = ArithmeticSequence(10, 2)  # 初始化一个从10开始，步长为2的实例
+print(s[4])  # 询问s[4]这个key的值，只会调用__getitem__方法
+s[20] = 1  # 将s[20]这个key的值赋为1，只会调用__setitem__方法
