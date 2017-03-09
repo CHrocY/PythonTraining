cb71bcc109745e0a61471e8ca3d3cc9781769e07
CHrocY <13308196164@163.com>
2017-03-09 14:56:54 +0800

    Add a new file 'MemberAcess.py

---
+++ DayTrainingOfPython/3.5/Study/MemberAcess.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author = Cheny
+
+
+class ArithmeticSequence:
+	def __init__(self, start=0, step=1):
+		self.start = start
+		self.step = step
+		self.changed = {}
+
+	# 使用静态方法，把本来放在类外面的函数放入类中来，
+	# 有利于命名空间的整洁和代码的美观～也许吧～
+	@staticmethod
+	def checkIndex(key):
+		if not isinstance(key, int): raise TypeError
+		if key < 0: raise IndexError
+
+	def __getitem__(self, key):
+		"""获取值的时候自动调用这个方法"""
+		ArithmeticSequence.checkIndex(key)
+		# 尝试获取值
+		try: return self.changed[key]
+		# 没有值就计算值
+		except KeyError:
+			print("calculate new value")
+			return self.start + key*self.step
+
+	def __setitem__(self, key, value):
+		"""给这个实例化的类赋值的时候自动调用这个方法"""
+		ArithmeticSequence.checkIndex(key)
+		self.changed[key] = value
+
+s = ArithmeticSequence(10, 2)
+print(s[4])
+s[4] = 1
+print(s[4])
